b6516e1be1be468905ac78165993ed76ce73a2cd
soehtetaung001 <scm.soehtetaung@gmail.com>
2020-12-12 22:09:45 +0630

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel SPA</title>
+    </head>
+    <body>
+        <h1> Laravel + Vue (SPA)
+
+        <div id="app">
+            <example-component></example-component>
+        </div>
+
+        <script src="/js/app.js"></script>
+    </body>
+</html>
